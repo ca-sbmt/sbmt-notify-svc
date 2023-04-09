6b4381f6a785cbc27221a7413ad6fa6be69a7b87
Guru Kallam <guru.kallam@gmail.com>
2023-04-09 12:23:03 -0700

    updates

--- chalice_app/app.py
+++ chalice_app/app.py
@@ -1,10 +1,16 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+
 from chalice import Chalice
 from app.main import app
 from mangum import Mangum
 
-chalice_app = Chalice(app_name='sbmt-notify-svc')
+chalice_app = Chalice(app_name='')
 handler = Mangum(app)
 
 @app.lambda_function()
 def main(event, context):
     return handler(event, context)
+
